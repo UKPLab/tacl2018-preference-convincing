7de892e4137a6be96d3ccaaaea689b7fd63176f4
edwinrobots <edwin.simpson@gmail.com>
2018-11-24 02:45:46 +0100

    switch lower bound computation off
    Former-commit-id: 0a5071e813dc91f949b8a20b202fe54ef66556e8

--- python/analysis/habernal_comparison/personalised_tests.py
+++ python/analysis/habernal_comparison/personalised_tests.py
@@ -52,7 +52,7 @@ class PersonalisedTestRunner(TestRunner):
 
         self.model = CollabPrefLearningSVI(nitem_features=self.ndims, ls=self.ls_initial, verbose=self.verbose,
                      nfactors=nfactors, rate_ls = 1.0 / np.mean(self.ls_initial),
-                     use_common_mean_t=common_mean, max_update_size=1000, use_lb=True,
+                     use_common_mean_t=common_mean, max_update_size=1000, use_lb=False,
                      shape_s0=shape_s0, rate_s0=rate_s0, ninducing=M)
 
         self.model.max_iter = 200
