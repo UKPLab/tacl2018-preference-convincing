afa4a78273a150a527c664ad911f21cb38143936
edwinrobots <edwin.simpson@gmail.com>
2019-02-20 19:09:44 +0100

    rename for retesting
    Former-commit-id: 6baeff8c0c321434fa219012b4ef1d761f4e9a7e

--- python/analysis/habernal_comparison/personalised_tests.py
+++ python/analysis/habernal_comparison/personalised_tests.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
 
     test_to_run = int(sys.argv[1])
 
-    test_dir = 'personalised_recheck'
+    test_dir = 'personalised_Qfix1'
 
     dataset_increment = 0
     # UKPConvArgCrowdSample tests prediction of personal data.
