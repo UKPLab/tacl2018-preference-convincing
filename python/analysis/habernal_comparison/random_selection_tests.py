69b948aea1ccabfdd4b99a69b69c567442b82b66
edwinrobots <edwin.simpson@gmail.com>
2019-05-30 03:22:44 +0200

    fix for crowdbt

--- python/analysis/habernal_comparison/random_selection_tests.py
+++ python/analysis/habernal_comparison/random_selection_tests.py
@@ -431,7 +431,7 @@ if __name__ == '__main__':
 
     test_to_run = int(sys.argv[1])
 
-    test_dir = 'rand_sel_debug1'
+    test_dir = 'randsel1'
 
     # UKPConvArgCrowdSample tests prediction of personal data.
     # UKPConvArgCrowdSample_evalMACE uses the personal data as input, but predicts the global labels/rankings.
