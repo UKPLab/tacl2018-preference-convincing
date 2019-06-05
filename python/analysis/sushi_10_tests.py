51fe3a3c62b1ef96ddf229917c6b9e6254e4c4f6
edwinrobots <edwin.simpson@gmail.com>
2019-06-06 01:42:32 +0200

    back to normal sushi

--- python/analysis/sushi_10_tests.py
+++ python/analysis/sushi_10_tests.py
@@ -117,7 +117,7 @@ def run_crowd_GPPL(u_tr, i1_tr, i2_tr, ifeats, ufeats, prefs_tr,
     # TODO check whether this setup still works for Sushi-B tests. Then run on conv tests -- probably need to tune sy hyperparameters
     # TODO test with the original selection of user inducing points again.
 
-    model = CollabPrefLearningSVI(ifeats.shape[1], ufeats.shape[1], mu0=0, shape_s0=shape_s0, rate_s0=rate_s0 / Nfactors,
+    model = CollabPrefLearningSVI(ifeats.shape[1], ufeats.shape[1], mu0=0, shape_s0=shape_s0, rate_s0=rate_s0,
                                   shape_sy0=1e10 if sushiB else 1e10, rate_sy0=1e10 if sushiB else 1e10, ls=None,
                                   nfactors=Nfactors, ninducing=ninducing, max_update_size=max_update_size,
                                   forgetting_rate=forgetting_rate, verbose=verbose, use_lb=True,
