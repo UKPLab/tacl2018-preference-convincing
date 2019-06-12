5f9b5a9fb081a91508a1e01725e7e129a96f4a6a
edwinrobots <edwin.simpson@gmail.com>
2019-06-12 02:59:40 +0200

    try with 10 times ls

--- python/models/collab_pref_learning_vb.py
+++ python/models/collab_pref_learning_vb.py
@@ -243,12 +243,12 @@ class CollabPrefLearningVB(object):
         else:
             self.conv_threshold = 1e-2
 
-        self.conv_check_freq = 4
+        self.conv_check_freq = 2
 
         self.max_iter_G = 10
         self.max_iter = 200
         self.min_iter = 1
-        self.n_converged = 5  # number of iterations while apparently converged (avoids numerical errors)
+        self.n_converged = 10  # number of iterations while apparently converged (avoids numerical errors)
         self.vb_iter = 0
 
         self.verbose = verbose
