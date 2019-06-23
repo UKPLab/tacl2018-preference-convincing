6074fbf7fb213309d0e2ccdc7f6760e0758666d1
edwinrobots <edwin.simpson@gmail.com>
2019-06-23 15:22:39 +0200

    fix to without consensus

--- python/analysis/sushi_10_tests.py
+++ python/analysis/sushi_10_tests.py
@@ -979,8 +979,8 @@ if __name__ == '__main__':
 
         # Repeat 25 times... Run each method and compute its metrics.
         methods = [
-                   #'khan',
-                   'crowd-GPPL',
+                   'khan',
+                   #'crowd-GPPL',
                    # 'crowd-GPPL\\u',
                    #'crowd-BMF',
                    #'crowd-GPPL\\u-noConsensus', # Like Houlsby CP (without user features)
