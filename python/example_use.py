fix and examples for diagonal covariance

python/example_use.py
@@ -122,19 +122,15 @@ def train_model(embeddings):
     items_feat, n_ling_feats, word_to_indices_map, a1_train, a2_train, prefs_train, ndims \
        = load_train_dataset(training_dataset, embeddings)  # reload only if we use a new dataset
 
-    ls_initial = compute_median_lengthscales(items_feat)
-
-    model = GPPrefLearning(ninput_features=ndims, ls_initial=ls_initial, verbose=False,
-                                shape_s0=2.0, rate_s0=200.0, rate_ls=1.0 / np.mean(ls_initial),
-                                use_svi=True, ninducing=500, max_update_size=200, kernel_combination='*',
-                                forgetting_rate=0.7, delay=1.0)
+    model = GPPrefLearning(ninput_features=ndims, verbose=False, shape_s0=2.0, rate_s0=200.0, use_svi=True,
+                           ninducing=500, max_update_size=200, kernel_combination='*', forgetting_rate=0.7, delay=1.0)
 
     model.max_iter_VB = 2000
 
     print("no. features: %i" % items_feat.shape[1])
 
     model.fit(a1_train, a2_train, items_feat, np.array(prefs_train, dtype=float) - 1, optimize=False,
-              input_type='zero-centered')
+              input_type='zero-centered', use_median_ls=True)
 
     logging.info("**** Completed training GPPL ****")
 

python/models/gp_classifier_svi.py
@@ -185,7 +185,7 @@ class GPClassifierSVI(GPClassifierVB):
         # self.u_invSm = np.zeros((self.ninducing, 1), dtype=float)  # theta_1
 
         if self.cov_type == 'diagonal':
-            self.u_invS = self.invK_mm * self.shape_s0 / self.rate_s0
+            self.u_invS = np.ones(self.ninducing) * self.shape_s0 / self.rate_s0
             # self.u_invS = np.zeros((self.ninducing), dtype=float)  # theta_2
             self.u_Lambda = np.zeros((self.ninducing), dtype=float) # observation precision at inducing points
         else:
@@ -393,12 +393,14 @@ class GPClassifierSVI(GPClassifierVB):
         #(self.K_nm / self.s).dot(self.s * self.invK_mm).dot(self.uS).dot(self.u_invSm)
         if self.cov_type == 'diagonal':
             if self.um_minus_mu0.size != mu0.size:
-                logging.error('We cannot make predictions for new test items when using a diagonal covariance -- we '
-                              'need to be able to use the features to make predictions.')
-                if Ks_nn is not None:
-                    return np.zeros(mu0.size), np.diag(np.ones(mu0.size)/self.s)
+                # logging.error('We cannot make predictions for new test items when using a diagonal covariance -- we '
+                #               'need to be able to use the features to make predictions.')
+                if Ks_nn is not None and full_cov:
+                    return np.zeros((mu0.size, 1)), np.diag(np.ones(mu0.size)/self.s)
+                elif Ks_nn is not None and not full_cov:
+                    return np.zeros((mu0.size, 1)), np.ones((mu0.size, 1))/self.s
                 else:
-                    return np.zeros(mu0.size)
+                    return np.zeros((mu0.size, 1))
 
             fhat = self.um_minus_mu0 + mu0
             if Ks_nn is not None and full_cov:
@@ -469,7 +471,7 @@ class GPClassifierSVI(GPClassifierVB):
 
         if self.covpair is None:
             if self.cov_type == 'diagonal':
-                self.covpair = 1.0
+                self.covpair = np.eye(self.ninducing)
             else:
                 self.covpair = scipy.linalg.solve(self.Ks_mm, self.Ks_nm.T).T
 